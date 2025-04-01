98a4ff9bc1f9d7a55312d51a48e316c4243a4d05
sofiagorgees <143371363+sofiagorgees@users.noreply.github.com>
2025-04-01 17:15:53 -0400

    fixed bugs datetime_convert.py

--- programming files/datetime_convert.py
+++ programming files/datetime_convert.py
@@ -3,9 +3,13 @@ from datetime import datetime
 # Initialize date string
 date_str = "2022-03-17 10:45:30"
 # Initialize date object
-date_obj = dateime.strptime(date_str '%Y-%m-%d %H:%M:%S')
+#bug fixed added t to datetime
+#bug fixed added comma
+date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
 # Format date
+#bug fixed added comma
 formatted_date = date.strftime('%m/%d/%Y %H:%M:%S')
 
 # Print Formatted date
-formatted_date
+#added print
+print(formatted_date)
