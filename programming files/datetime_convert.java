acd458df0cc2babbf401ce6748e0269aee64a71d
tarabayliss <143192455+tarabayliss@users.noreply.github.com>
2025-04-01 17:16:30 -0400

    Update datetime_convert.java

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -7,9 +7,13 @@ public class datetime_convert {
         String date = "2022-03-17 10:45:30";
         //initalize formatter as a DateTimeFormatter object
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+       
         //initalize dateObj as a localDateTime object
+        //Missing a semi colon, syntax error
         LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter)
+       
         //initalize and define formattedDate as a string that is the formatted date
+        //Missing a semi colon, syntax error
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"))
 
         //print out formattedDate
