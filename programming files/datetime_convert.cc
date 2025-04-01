2768170bd1198cc9e9e1b31ee88ca1ca4fe296ee
Sorin <sorinja50@gmail.com>
2025-04-01 17:25:48 -0400

    Fixed things

--- programming files/datetime_convert.cc
+++ programming files/datetime_convert.cc
@@ -6,7 +6,7 @@
 int main()
 {
     // Initialize date_str with date and time
-    string date_str = "2022-03-17 10:45:30";
+    std::string date_str = "2022-03-17 10:45:30";
 
     // Create structure to hold date
     std::tm date_obj = {};
@@ -19,10 +19,11 @@ int main()
 
     // Format string
     std::stringstream formatted_date_ss;
-    formatted_date_ss << std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
+    formatted_date_ss == std::put_time(&date_obj, "%m/%d/%Y %H:%M:%S");
     std::string formatted_date = formatted_date_ss.str();
 
     // Output formatted date
     std::cout << formatted_date << std::endl;
 
     return 0
+}
